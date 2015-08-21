f8bd2717bd4396159e55b45a705c7dea759a7e7b
erwai <bdlas321@gmail.com>
2015-08-21 20:47:48 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+	if(empty($_POST)){
+		exit;
+	}
+	switch ($_POST['operation']) {
+		case 'letter_in':
+			do_mysql_work();
+			http_response_code(200);
+			break;
+		
+		default:
+			# code...
+			break;
+	}
+	function do_mysql_work(){
+
+	}
+?>
