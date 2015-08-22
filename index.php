d24e91670e0d7c183434f8a45926e9a9ff5f8eb5
erwai <bdlas321@gmail.com>
2015-08-22 09:53:38 +0800

    add letter_receiving operation to index.php  and simulating successfully.

--- index.php
+++ index.php
@@ -1,27 +1,15 @@
 <?php
+	require('./function.php');
 	if(empty($_POST)){
 		exit();
 	}
 	switch ($_POST['operation']) {
-		case 'letter_in':
-			do_mysql_work();
-			http_response_code(200);
-			break;
-		case 'for_test':
-			if($_POST['option']=="A"){
-				$data['returning'] = "this is returning!";
-				$data['type'] = "A";
-			}else{
-				$data['returning'] = "else else and else~";
-				$data['type'] = "BCDFG";
-			}
-			echo json_encode($data);
+		case 'letter_receiving':
+			$working = new do_mysql();
+			echo $working->insert_data($_POST['order'], $_POST['data']);
 			break;
 		default:
 			# code...
 			break;
-	}
-	function do_mysql_work(){
-
 	}
 ?>
